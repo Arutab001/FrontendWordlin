779bd499d0faa25e8a84226936cf7bbf24861f8a
maksimblankov <maxblankov436@gmail.com>
2022-11-08 13:20:12 +0200

    create page choice_of_languages

---
+++ src/choiceOfLanguages.js
@@ -0,0 +1,9 @@
+(async () => {
+
+})();
+
+document.getElementById('submit').addEventListener('click', async () => {
+
+    console.log(document.getElementById('nativeLanguage').value)
+    console.log(document.getElementById('targetLanguage').value)
+})
